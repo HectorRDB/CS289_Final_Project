f53da5ed99871c158e6eca7bedc7177cdd2bb591
Ariane Lozac'hmeur <ariane.lozachmeur@gmail.com>
2018-05-03 21:24:34 -0700

    Update of the readme

--- Code/README.md
+++ Code/README.md
@@ -1,1 +1,66 @@
 Here will go all the various scripts
+
+
+-------------------------------------------
+
+simulation.py
+
+Script generating a random function to test our methods.
+
+Functions:
+
+get_dataset(n_genes, n_times, p_dropped, var=2, noise='gaussian', save=False)
+Inputs:
+-n_genes: int
+The number of function generated
+
+-n_times: int
+The number of timestamps
+
+-p_dropped: tuple of floats in [0,1], default=(0.15,0.3)
+The range for the proportion of dropped points. For each function, the actually number of dropped points will be selected uniformly in this range.
+
+-var: float, default=2
+Variance of the noise added to the data
+
+-noise: str, default='gaussian'
+Type of noise added to the data. For now, only gaussian noise is supported.
+
+-save: boolean, default=False
+Save or visualise the dataset.
+If true the following files are saved in the cache: 'synthetic_data.csv', 'times.csv', 'dropped_points.csv', 'generative_functions.csv'
+
+Outputs:
+-values: pandas.DataFrame
+A DataFrame of size n_genes x n_times containing the noisy data
+
+-times: numpy.array
+A vector of size n_times containing the sorted timestamps corresponding to values
+
+-classes: pandas.DataFrame
+A DataFrame of size n_genes x n_times containing the class of the data
+
+--------
+
+plot_functions(functions, values, times, classes)
+Plot on graph per function with on the same graph the real function, the noisy values and colors the 2 classes.
+
+Inputs:
+-functions: list
+A list of functions to graph. Ideally they should be constructed from the coefficients in generative_functions.csv, using numpy.polyval to generate the function from the coefficients.
+
+- values: pandas.DataFrame
+A DataFrame containing the noisy values corresponding the the functions (each row of the DataFrame contains the values of a function).
+
+- times: pandas.DataFrame
+A DataFrame containing the timestamps of the points
+
+- classes. pandas.DataFrame
+A DataFrame containing the classes of the points. Used to color the graph. Red points are dropped/false zeros and green points are real values.
+
+Output:
+None
+
+
+-------------------------------------------
+
--- Code/simulation.py
+++ Code/simulation.py
@@ -4,33 +4,33 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-def get_function(d=3):
+def _get_function(d=3):
     # Generates a function of degree d.
 
     coeff = np.random.uniform(-10, 10, size=d)
     return lambda x: np.polyval(coeff, x), coeff
 
 
-def get_functions(n, d):
+def _get_functions(n, d):
     # Generates n functions of degree d with a least one positive value on [0,1]
 
     functions = []
     coefficients = pd.DataFrame()
     while len(functions) < n:
-        f, coeff = get_function(d)
+        f, coeff = _get_function(d)
         if np.max(f(np.arange(0, 1, 0.1))) > 0:
             functions.append(f)
             coefficients = coefficients.append(pd.Series(coeff), ignore_index=True)
     return functions, coefficients
 
 
-def get_times(n_times=500):
+def _get_times(n_times=500):
     # Generates n_times timestamps between [0,1] to evaluate the function
 
     return np.sort(np.random.uniform(0, 1, size=n_times))
 
 
-def add_noise(values, var=2, type='gaussian'):
+def _add_noise(values, var=2, type='gaussian'):
     # Adds a noise of type type to the values with a variance of var.
     # This function also operates the cut-of to 0 (we need only positive values for biological significance).
 
@@ -44,7 +44,7 @@ def add_noise(values, var=2, type='gaussian'):
     return noisy_values
 
 
-def evaluate_functions(functions, times):
+def _evaluate_functions(functions, times):
     # Evaluates the functions at each timestamp.
     # Returns a DataFrame containing the values of the functions.
 
@@ -55,7 +55,7 @@ def evaluate_functions(functions, times):
     return values
 
 
-def drop_points(values, range=(0.2, 0.4)):
+def _drop_points(values, range=(0.2, 0.4)):
     # Drops a random number of points that are not already 0 (in the specified range)
     # Returns both the values (after the drop) and the classes of each point (0: not dropped, 1: dropped).
 
@@ -93,12 +93,12 @@ def plot_functions(functions, values, times, classes):
 def get_dataset(n_genes, n_times, p_dropped, var=2, noise='gaussian', save=False):
     # Generates the dataset. If save=True, saves the coefficients of the functions, the noisy values and the classes.
 
-    functions, coefficients = get_functions(n=n_genes, d=degree)
-    times = get_times(n_times)
-    values = evaluate_functions(functions, times)
-    noisy_values = add_noise(values, var, noise)
+    functions, coefficients = _get_functions(n=n_genes, d=degree)
+    times = _get_times(n_times)
+    values = _evaluate_functions(functions, times)
+    noisy_values = _add_noise(values, var, noise)
     noisy_values[noisy_values < 0] = 0
-    dropped_values, classes = drop_points(noisy_values, p_dropped)
+    dropped_values, classes = _drop_points(noisy_values, p_dropped)
     if save:
         pd.DataFrame(times).transpose().to_csv('../cache/times.csv')
         coefficients.to_csv(('../cache/generative_functions.csv'))
@@ -106,10 +106,10 @@ def get_dataset(n_genes, n_times, p_dropped, var=2, noise='gaussian', save=False
         classes.to_csv(('../cache/dropped_points.csv'))
     else:
         plot_functions(functions, dropped_values, times, classes)
-    return values, classes
+    return values, times, classes
 
 
-n_genes = 1000
+n_genes = 5
 degree = 12
 n_times = 500
 var = 2
@@ -124,4 +124,4 @@ if __name__ == '__main__':
     # values, classes = drop_points(values)
     # plot_functions(functions, values, times)
 
-    get_dataset(n_genes, n_times, p_dropped=(0.15, 0.3), save=True)
+    get_dataset(n_genes, n_times, p_dropped=(0.15, 0.3), save=False)
